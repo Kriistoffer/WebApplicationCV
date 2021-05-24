0dd06b1f63e82887c7959c97b3a5aba597fa734f
Kristoffer <kjohansson93@hotmail.se>
2021-05-24 23:51:12 +0200

    Added start page and typewriter effect, aligned logos on navbar, edited title in index.html

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import './Home.css';
+import TypeWriterEffect from 'react-typewriter-effect';
 
 export class Home extends Component {
     render() {
         return (
             <div className="home-section" id="home">
-                <h1 className="title">This is my home page</h1>
+                <div className="intro-firstname">
+                    Kristoffer
+                </div>
+                <div className="intro-lastname">
+                    Johansson
+                </div>
+                <div classname="intro-subtext">
+                <TypeWriterEffect
+                        textStyle={{ fontFamily: '',
+                        color: "white",
+                        fontSize: "2em" }}
+                        startDelay={1500}
+                        cursorColor="white"
+                        text="Studerande inom objektorienterad programmering..</>"
+                        typeSpeed={100}
+                        scrollArea={document} />
+                </div>
             </div>
         );
     }
