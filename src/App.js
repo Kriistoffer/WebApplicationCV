0b3f168bb201092ac83e74f45e2854c3e6059428
Kriistoffer <kjohansson93@hotmail.se>
2021-06-05 21:19:26 +0200

    Changed the navigation links to proper button components. Made the navigation links scroll to the correct page component.

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
       <Home/>
       <About/>
       <Work/>
-      <Education/>
-      <Qualifications/>
+      {/* <Education/> */}
+      {/* <Qualifications/> */}
       <Projects/>
     </div>
   );
