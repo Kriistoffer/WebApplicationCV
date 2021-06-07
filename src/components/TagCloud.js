2c0492ea7b2a5210625d956fb594982c71df565c
Kriistoffer <kjohansson93@hotmail.se>
2021-06-07 13:24:32 +0200

    Added a word cloud component and made some preparations for deployment.

---
+++ src/components/TagCloud.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { TagCloud } from 'react-tagcloud';
+import './TagCloud.css';
+
+
+const data = [
+    { value: 'JavaScript', count: 12 },
+    { value: 'C#', count: 30 },
+    { value: 'C++', count: 20 },
+    { value: 'SQL', count: 19 },
+    { value: 'LaTeX', count: 13 },
+    { value: 'Python', count: 2 },
+    { value: 'HTML', count: 24 },
+    { value: 'CSS', count: 22 },
+    { value: 'React', count: 16 },
+    { value: 'Visual Basic', count: 2 },
+    { value: 'Java', count: 20 },
+    { value: 'Git', count: 22 },
+    { value: 'UML', count: 18 },
+]
+
+//  const WordCloud = () => (
+//     <TagCloud
+//         minSize={12}
+//         maxsize={35}
+//         tags={data}
+//         className="simple-cloud" />
+// )
+
+// export default WordCloud;
+
+export default () => (
+    <TagCloud tags={data} minSize={16} maxSize={55} className="simple-cloud" />
+  )
